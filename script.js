0618d03ff2319ea56b49a44d0cb8c24cc9c634bf
Anderson Queiroz da Silva <52304311+AndersonS7@users.noreply.github.com>
2023-05-07 13:26:12 -0300

    finalizado a lógica e estilização do menu responsivo

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const logo = document.querySelector('#logo'); 
-const img_second = document.querySelector('#img_second'); 
+const logo = document.querySelector('#logo');
+const img_second = document.querySelector('#img_second');
 
 const observer = new ResizeObserver(entries => {
     for (let entry of entries) {
         const { width } = entry.contentRect;
 
-        if(width < 715){
+        if (width <= 768) {
             logo.src = './assets/icons/logo-white.svg';
             img_second.src = './assets/imgs/mobile/secundaria-rosa.png';
-        }else{
+        } else {
             logo.src = './assets/icons/logo.svg';
             img_second.src = './assets/imgs/web/secundária-rosa.png';
         }
-
-
     }
 });
+
+function menuShow() {
+    let menuMobile = document.querySelector('.menu_mobile');
+    console.log(menuMobile)
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
 
 observer.observe(document.body);
