0daba6550270cb6086b955a3084e562a0da57187
Anderson Queiroz da Silva <52304311+AndersonS7@users.noreply.github.com>
2023-05-16 22:30:53 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const logo = document.querySelector('#logo');
-const img_second = document.querySelector('#img_second');
+const img_second = document.querySelector('#img_second_about');
 const btn_mobile_icon = document.querySelector("#btn_mobile_icon");
 let mobileBtnOn = false;
 
@@ -7,6 +7,13 @@ const observer = new ResizeObserver(entries => {
     for (let entry of entries) {
         const { width } = entry.contentRect;
 
+        if (width >= 1470 || width <= 768) {
+            img_second.src = "./assets/imgs/mobile/secundaria-rosa.png"
+        } else {
+            img_second.src = "./assets/imgs/web/secundária-rosa.png"
+        }
+
+        // telas noramais
         if (width <= 768 && mobileBtnOn) {
 
             logo.src = './assets/icons/logo.svg';
